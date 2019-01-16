40cef89084187cabf005dc0f965afadf07038dd7
L-Train <Ltrain23!>
2019-01-16 13:55:20 -0500

    update

--- notes.js
+++ notes.js
@@ -1,8 +1,35 @@
 console.log('Notes.js ', 'Starting notes.js')
 
-var addNote = ( title, body) => {
+const fs = require('fs');
 
-    console.log('Adding note', title, body)
+var fectchNotes = () => {
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json')
+        return JSON.parse(notesString)
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
+var addNote = (title, body) => {
+    //console.log('Adding note', title, body)
+    var notes = fectchNotes();
+    var note = {
+        title: title,
+        body: body
+    }
+    var duplicateNotes = notes.filter((note) => note.title === title)
+    if (duplicateNotes.length === 0) {
+        notes.push(note)
+        saveNotes(notes)
+        return note;
+    }
 }
 
 var getAll = () => {
@@ -11,19 +38,34 @@ var getAll = () => {
 }
 
 var getNote = (title) => {
-
-    console.log('Getting note', title)
+    //console.log('Getting note', title)
+    var notes = fectchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title)
+    return filteredNotes[0]
 }
 
 var removeNote = (title) => {
+    //console.log('Remove note', title)
+    // fetch notes
+    var notes = fectchNotes();
+    // filter notes, remove the one with title of argument
+    var filteredNotes = notes.filter((note) => note.title !== title)
+    // save new notes array
+    saveNotes(filteredNotes)
 
-    console.log('Remove note', title)
+    return notes.length !== filteredNotes.length;
 }
 
+var logNote = (note) => {
+    console.log('---')
+    console.log(`Title: ${note.title}` )
+    console.log(`Body: ${note.body}` )
+}
 
 module.exports = {
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 }
